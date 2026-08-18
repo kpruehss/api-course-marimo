78bd6d10442b16d7cbf26c1a92e91324ef78de3f
Karsten Pruehss <karsten.pruehss@tabcorp.com.au>
2026-08-18 11:51:24 +1000

    install base packages

--- 001_requests.py
+++ 001_requests.py
@@ -1,7 +1,8 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#     "marimo>=0.24.0",
+#     "marimo>=0.23.3",
+#     "python-dotenv>=1.2.3",
 # ]
 # ///
 
@@ -11,12 +12,12 @@ __generated_with = "0.23.9"
 app = marimo.App()
 
 
-app._unparsable_cell(
-    r"""
-    uv add anthropic
-    """,
-    name="_"
-)
+@app.cell
+def _():
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    return
 
 
 if __name__ == "__main__":
