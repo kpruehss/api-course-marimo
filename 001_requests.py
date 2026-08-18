540e6756ebf28dc7bddb906b7107b9261043b978
Karsten Pruehss <karsten.pruehss@tabcorp.com.au>
2026-08-18 13:32:40 +1000

    make a first request

--- 001_requests.py
+++ 001_requests.py
@@ -1,6 +1,7 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
+#     "anthropic>=0.122.0",
 #     "marimo>=0.23.3",
 #     "python-dotenv>=1.2.3",
 # ]
@@ -14,9 +15,50 @@ app = marimo.App()
 
 @app.cell
 def _():
+    import dotenv
+    import anthropic
+
+    return
+
+
+@app.cell
+def _():
     from dotenv import load_dotenv
 
     load_dotenv()
+    return
+
+
+@app.cell
+def _():
+    # Setup the client
+    from anthropic import Anthropic
+
+    client = Anthropic()
+    model = "claude-sonnet-5"
+    return client, model
+
+
+@app.cell
+def _(client, model):
+    # Make a request
+    import token
+    message = client.messages.create(
+      model=model,
+      max_tokens=1000,
+      messages=[
+        {
+          "role": "user",
+          "content": "What is quantum computing? Answer in 1 sentence"
+        }
+      ]
+    )
+    return (message,)
+
+
+@app.cell
+def _(message):
+    message
     return
 
 
